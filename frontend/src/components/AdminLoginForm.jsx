ab761a0394578d8669456bd74641ef212bf29f35
Sol Urbano <surbano@fi.uba.ar>
2025-05-03 18:58:25 -0300

    Feat; add frontend login

---
+++ frontend/src/components/AdminLoginForm.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import api from '../api/axios';
+import '../styles/LoginForm.css'; // Importamos el archivo CSS
+
+function AdminLoginForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      const res = await api.post('/auth/login', { username, password });
+      alert('Login exitoso');
+      localStorage.setItem('token', res.data.access_token);
+    } catch (err) {
+      console.error(err);
+      alert('Login fallido');
+    }
+    setIsLoading(false);
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-card">
+        <h2 className="login-title">Login Admin</h2>
+        <form onSubmit={handleLogin} className="login-form">
+          <input
+            type="text"
+            className="login-input"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            className="login-input"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button type="submit" className="login-button" disabled={isLoading}>
+            {isLoading ? 'Iniciando...' : 'Iniciar Sesión'}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default AdminLoginForm;
