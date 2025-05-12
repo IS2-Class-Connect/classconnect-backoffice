f5ee7879e8033f9ee073d1ac688b1190a52d4da1
Sol Urbano <surbano@fi.uba.ar>
2025-05-12 18:32:04 -0300

    fix: incorrect token being sent

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -9,10 +9,7 @@ const api = axios.create({
 
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
-    }
+    config.headers['Authorization'] = `Bearer admin-token`;
     return config;
   },
   (error) => Promise.reject(error)
